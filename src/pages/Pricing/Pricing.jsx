4c5038daac91bd5a00762b910b32d3445840d2ce
Navin <95266285+NavinLamsal@users.noreply.github.com>
2024-07-14 20:42:07 +0545

    Update Pricing.jsx

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -1,40 +1,19 @@
 import React from "react";
-import { useSelector } from "react-redux";
+
 import Divider from "../../components/divider/Divider";
-import Breadcrumb from "../../components/breadcrumb/Breadcrumb";
+
 import DashboardLayout from "../../components/Layout/DashboardLayout";
-import LandingLayout from "../../components/Layout/LandingLayout";
+
 import PricingPlans from "./PricingPlans";
 
 const PricingPage = () => {
-  const user = useSelector((state) => state.userReducer);
 
-  if (user.isAuthenticated) {
     return (
       <DashboardLayout title={"Pricing plans"}>
         <PricingPlans />
         <Divider />
       </DashboardLayout>
     );
-  } else {
-    return (
-      <LandingLayout>
-        <Breadcrumb
-          breadcrumbTitle="Pricing plans"
-          breadcrumbNav={[
-            {
-              navText: "Home",
-              path: "/",
-            },
-          ]}
-        />
-        <div className="container">
-          <PricingPlans />
-        </div>
-        <Divider />
-      </LandingLayout>
-    );
-  }
 };
 
 export default PricingPage;
