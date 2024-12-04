ade8583d70a75fb81c6e07cea36b03bf9eb56c41
navinlamsal <navinlamsal11@gmail.com>
2024-12-04 12:35:58 +0545

    plan purchase error fixed

--- src/pages/Pricing/Pricing.jsx
+++ src/pages/Pricing/Pricing.jsx
@@ -8,38 +8,6 @@ import PricingPlans from "./PricingPlans";
 import { useDispatch } from "react-redux";
 import { planDetailSuccess } from "../../redux/slices/membershipTypeSlice";
 
-
-// const planConfig = {
-//   2: {
-//     cardClass: "pricingcard-starter",
-//     textColorClass: "pricing-text-color",
-//     bgColorClass: "bg-gray text-warning",
-//     priceColorClass: "text-dynamic-white",
-//     discountsColorClass: "text-primary",
-//   },
-//   4: {
-//     cardClass: "pricingcard-gold",
-//     textColorClass: "pricingcard-text-gold",
-//     bgColorClass: "pricingcard-premium text-white",
-//     priceColorClass: "text-white",
-//     discountsColorClass: "text-black",
-//   },
-//   3: {
-//     cardClass: "pricingcard-premium",
-//     textColorClass: "text-white",
-//     bgColorClass: "bg-warning text-white",
-//     priceColorClass: "pricing-text-color",
-//     discountsColorClass: "text-warning",
-//   },
-//   1: {
-//     cardClass: "pricingcard-platinum",
-//     textColorClass: "text-danger",
-//     bgColorClass: "bg-gold text-black",
-//     priceColorClass: "text-danger",
-//     discountsColorClass: "text-black",
-//   },
-// }
-
 const PricingPage = () => {
   const dispatch= useDispatch()
 
